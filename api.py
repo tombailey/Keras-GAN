47ac50d56141fc00a5a4affe9c5fda818824aa78
Tom Bailey <tombailey@users.noreply.github.com>
2021-12-19 22:50:00 +0900

    Add health endpoint

--- api.py
+++ api.py
@@ -29,6 +29,9 @@ def generate_skins():
         )
         return send_file(join(temporary_directory.name, 'generated.png'))
 
+@app.route('/health', methods=['GET'])
+def health():
+    return {}, 200
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
